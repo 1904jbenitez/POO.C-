5d54347540202e4d7d293cdb6a2f9f030e2c453f
1904jbenitez <jbenitez@arfp.asso.fr>
2021-06-29 10:18:58 +0200

    creation du dossier C#

---
+++ CompteBancaire/CompteBancaire/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace CompteBancaire
+{
+    class Compte
+    {
+        private long numCli;
+        private double solde;
+        private string nomCli;
+        private double decouvertAutorise;
+        
+
+        public long NumCli                                              //Lecture seule
+        {
+            get => numCli;
+        }
+
+        public string NomCli
+        {
+            get => nomCli; set => nomCli = value;
+        }
+
+        public double Solde
+        {
+            get => solde; set => solde = value;
+        }
+
+        public string GetNomCli()
+        {
+            return this.nomCli;
+        }
+
+        public void SetSolde(double _montant)
+        {
+            this.solde = _montant;
+        }
+
+        // Constructeurs
+        public CompteClient(long _numCli, string _nomCli, double _solde, double _da)
+        {
+            this.numCli = _numCli;
+            this.nomCli = _nomCli;
+            this.SetSolde(_solde);
+            this.decouvertAutorise = _da;
+        }
+        //Destructeur
+        //methodes
+
+        public double DecouvertAutorise
+        {
+            get => decouvertAutorise; set => decouvertAutorise = value;
+        }
+
+
+        public void Crediter(double somme)
+        {
+            solde += somme;
+            Console.Out.WriteLine("Opération bien effectuée");
+        }
+
+        public void Crediter(Compte c, double somme)
+        {
+            if (c.solde >= somme)
+            {
+                c.solde -= somme;
+                solde += somme;
+                Console.Out.WriteLine("Opération bien effectuée");
+            }
+            else
+                Console.Out.WriteLine("Solde de compte insuffisant");
+        }
+
+        public void Debiter(double somme)
+        {
+            if (solde >= somme)
+            {
+                solde -= somme;
+                Console.Out.WriteLine("Opération bien effectuée");
+            }
+            else
+                Console.Out.WriteLine("Solde insuffisant");
+        }
+
+        public void Debiter(Compte c, double somme)                      //(surcharge des méthodes)
+        {
+            if (solde >= somme)
+            {
+                solde -= somme;
+                c.solde += somme;
+                Console.Out.WriteLine("Opération bien effectuée");
+            }
+            else
+                Console.Out.WriteLine("Solde insuffisant");
+        }
+
+        public void Afficher()
+        {
+            Console.Out.WriteLine("************************");
+            Console.Out.WriteLine("Numéro de Compte: " + numero);
+            Console.Out.WriteLine("Solde de compte: " + solde);
+            Console.Out.WriteLine("Propriétaire du compte : ");
+            proprio.Afficher();
+            Console.Out.WriteLine("*************************");
+        }
+
+        public static void Nombre_Comptes()
+        {
+            Console.Out.WriteLine("\n\nLe nombre de comptes crées: " + nombre_comptes);
+        }
+
+
+    }
+}
+
+
---
+++ Le Point/Le Point/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Le_Point
+{
+    
+           public class Point
+        {
+            //attributs...
+            private string nom;
+            private float axeAbscisseX;
+            private float axeOrdonneeY;
+            //propriétés...
+            public string Nom { get => nom; }
+            public float AxeAbscisseX { get => axeAbscisseX; }
+            public float AxeOrdonneeY { get => axeOrdonneeY; }
+            //constructeur...
+            public Point(string _nom, float _axeAbscisseX, float _axeOrdonneeY)
+            {
+                this.nom = _nom;
+                this.axeAbscisseX = _axeAbscisseX;
+                this.axeOrdonneeY = _axeOrdonneeY;
+            }
+            public Point()
+            {
+                this.nom = " ";
+                this.axeAbscisseX = 0;
+                this.axeOrdonneeY = 0;
+
+            }
+            //méthodes...
+            public string IndiquerPosition()
+            {
+                return ToString();
+            }
+
+
+            public void Deplacer(float _x, float _y)
+            {
+                this.axeAbscisseX = _x;
+                this.axeOrdonneeY = _y;
+            }
+
+            public override string ToString()
+            {
+                return "Le point appelé " + nom + " Abscisse est: " + axeAbscisseX + " Ordonnee est: " + axeOrdonneeY;
+            }
+
+
+            public Point ConstruireSymOrd(string _nom)
+            {
+                Point f = new Point();
+                f.nom = _nom;
+                f.axeAbscisseX = (this.axeAbscisseX);
+                f.axeOrdonneeY = -(this.axeOrdonneeY);
+
+                return f;
+            }
+
+
+            public Point ConstruireSymAbs(string _nom)
+            {
+                Point f = new Point();
+                f.nom = _nom;
+                f.axeAbscisseX = -(this.axeAbscisseX);
+                f.axeOrdonneeY = (this.axeOrdonneeY);
+
+                return f;
+
+            }
+
+
+            public Point ConstruireSymOr(string _nom)
+            {
+                Point f = new Point();
+                f.nom = _nom;
+                f.axeAbscisseX = -(this.axeAbscisseX);
+                f.axeOrdonneeY = -(this.axeOrdonneeY);
+
+                return f;
+            }
+
+
+            public void PermuterBissec()
+
+
+            {
+                float temp = this.axeAbscisseX;
+                this.axeAbscisseX = this.axeOrdonneeY;
+                this.axeOrdonneeY = temp;
+            }
+        }
+    }
+
